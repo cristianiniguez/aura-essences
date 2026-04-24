063afd4dcd501fc3c5274a83d571bbd71b4991a2
cristianiniguez <felizpc1@gmail.com>
2026-04-24 00:40:51 -0400

    added eslint recommended configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,29 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 
 const eslintConfig = defineConfig([
+  js.configs.recommended,
+  stylistic.configs.recommended,
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts'
   ]),
-]);
+  {
+    rules: {
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/jsx-quotes': ['error', 'prefer-single'],
+      '@stylistic/operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }]
+    }
+  }
+])
 
-export default eslintConfig;
+export default eslintConfig
